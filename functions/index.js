e4882f5e37d1d4bf47eb0baa2855fd6f53caf425
Cristian Ramírez <cristianrd92@gmail.com>
2025-06-17 23:56:36 -0400

    se actualizan funciones, se agrega vacaciones y dia libre

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
+const admin = require("firebase-admin");
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 const functions = require("firebase-functions");
 const { esFeriadoHoy } = require("./getFeriado");
+const { esVacaciones } = require("./getVacaciones");
 const { marcarEnBuk } = require("./markBuk");
 const { notifyTelegram } = require("./notifyTelegram");
 const users = require("./user-config");
+const db = admin.firestore();
 
 function programarMarcajeConDelay(sentido) {
   const sentidoTexto = sentido === 1 ? 'ENTRADA' : 'SALIDA';
@@ -18,10 +24,40 @@ function programarMarcajeConDelay(sentido) {
       await new Promise(resolve => setTimeout(resolve, delaySegundos * 1000));
 
       const feriado = await esFeriadoHoy();
-      const esVacaciones = false;
+      const vacacionesActivas = await esVacaciones()
 
-      if (feriado || esVacaciones) {
-        const motivo = feriado ? `feriado: *${feriado}*` : "vacaciones";
+      // Si ambas son falsas, verificamos si hay un día libre programado, si no, simplemente omitimos busqueda en bd
+      if (!feriado && !vacacionesActivas) {
+        const marcajeConfigDoc = await db.collection("config").doc("marcaje").get();
+        let esDiaLibreProgramadoHoy = false;
+        if (marcajeConfigDoc.exists && marcajeConfigDoc.data().fechaDiaLibreProgramado) {
+          const fechaDiaLibreTimestamp = marcajeConfigDoc.data().fechaDiaLibreProgramado;
+          const fechaDiaLibre = fechaDiaLibreTimestamp.toDate();
+
+          const today = new Date();
+
+          fechaDiaLibre.setHours(0, 0, 0, 0);
+          today.setHours(0, 0, 0, 0);
+
+          if (fechaDiaLibre.getTime() === today.getTime()) {
+            esDiaLibreProgramadoHoy = true;
+            console.log(`🗓️ Hoy es un día libre programado: ${fechaDiaLibre.toISOString().split('T')[0]}`);
+          } else if (fechaDiaLibre.getTime() < today.getTime()) {
+            console.log(`🗓️ Día libre programado vencido (${fechaDiaLibre.toISOString().split('T')[0]}), limpiando.`);
+            await db.collection("config").doc("marcaje").set({ fechaDiaLibreProgramado: admin.firestore.FieldValue.delete() }, { merge: true });
+          }
+        }
+      }
+
+      if (feriado || vacacionesActivas || esDiaLibreProgramadoHoy) {
+        let motivo = "desconocido";
+        if (feriado) {
+          motivo = `feriado: *${feriado}*`;
+        } else if (vacacionesActivas) {
+          motivo = "vacaciones";
+        } else if (esDiaLibreProgramadoHoy) {
+          motivo = "día libre";
+        }
         for (const user of users) {
           await notifyTelegram(user, `🚫 Hoy es ${motivo}. No se ejecutó el marcaje de ${sentidoTexto}`);
         }
@@ -46,3 +82,4 @@ exports.marcarSalida = programarMarcajeConDelay(2);  // 17:59 con delay aleatori
 exports.testTelegramNotification = require("./testTelegram").testTelegramNotification;
 exports.marcarEntradaManual = require("./manualCheckin").marcarEntradaManual;
 exports.marcarSalidaManual = require("./manualCheckin").marcarSalidaManual;
+exports.toggleFromTelegram = require("./toggleFromTelegram").toggleFromTelegram;
--- functions/user-config.js
+++ functions/user-config.js
@@ -1,14 +1,10 @@
 module.exports = [
   {
     nombre: "Cristian Ramírez",
-    email: "cramirezd@acid.cl",
     rut: "18041574-2",
-    password: "McLarenF1@",
     telegramBotToken: "5725513520:AAHQEvrX5_YxBg69HfM1dOA_ET-A0SNW94Y",
     telegramChatId: "-831075068",
     latitud: -35.424515,
-    longitud: -71.598343,
-    urlLogin: "https://acidlabs.buk.cl/users/sign_in",
-    urlPortal: "https://acidlabs.buk.cl/static_pages/portal"
+    longitud: -71.598343
   }
 ];
